a37d48a86972e6b6a9817890a86933718e4c4a65
ransispoe <ransispoegithub@ninjasoapray.com>
2022-01-29 19:48:16 -0800

    Add wheel zoom

--- src/Components/Canvas.tsx
+++ src/Components/Canvas.tsx
@@ -3,13 +3,69 @@ import { Stage, Layer } from "react-konva";
 import SkillCircle from "./SkillCircle";
 import TreeImage from "./TreeImage";
 
-const dragmove = (pos: any) => {
+const scaleBy = 1.1;
+var currentScale = 1;
+
+const width = 3956;
+const height = 3654;
+
+const overflowBounds = 1.2;
+
+const dragBound = (pos: any) => {
   // This limits how much you can drag the image
   // TODO: somehow make this dynamic based on loaded image size
   return {
-    x: Math.max(Math.min(pos.x, 0), -3956 + window.innerWidth),
-    y: Math.max(Math.min(pos.y, 0), -3654 + window.innerHeight),
+    x: Math.max(
+      Math.min(pos.x, 0),
+      -width * currentScale * overflowBounds + window.innerWidth
+    ),
+    y: Math.max(
+      Math.min(pos.y, 0),
+      -height * currentScale * overflowBounds + window.innerHeight
+    ),
   };
+};
+
+const wheelFunc = (e: any) => {
+  // stop default scrolling
+  e.evt.preventDefault();
+
+  const stage = e.target.getStage();
+  const oldScale = stage.scaleX();
+  const pointer = stage.getPointerPosition();
+
+  const mousePointTo = {
+    x: (pointer.x - stage.x()) / oldScale,
+    y: (pointer.y - stage.y()) / oldScale,
+  };
+
+  // how to scale? Zoom in? Or zoom out?
+  let direction = e.evt.deltaY > 0 ? 1 : -1;
+
+  // when we zoom on trackpad, e.evt.ctrlKey is true
+  // in that case lets revert direction
+  if (e.evt.ctrlKey) {
+    direction = -direction;
+  }
+
+  const newScale = direction > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+  currentScale = newScale;
+  console.log("newScale", newScale);
+
+  if (width * newScale < window.innerWidth) {
+    return;
+  }
+  if (height * newScale < window.innerHeight) {
+    return;
+  }
+
+  stage.scale({ x: newScale, y: newScale });
+
+  const newPos = {
+    x: pointer.x - mousePointTo.x * newScale,
+    y: pointer.y - mousePointTo.y * newScale,
+  };
+  stage.position(newPos);
 };
 
 // TODO: add zoom with scroll/pinch
@@ -18,7 +74,13 @@ export default function CanvasExample() {
   // TODO: make x and y dynamic based on the image
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer draggable x={-1200} y={-2100} dragBoundFunc={dragmove}>
+      <Layer
+        draggable
+        x={-1200}
+        y={-2100}
+        dragBoundFunc={dragBound}
+        onWheel={wheelFunc}
+      >
         <TreeImage src="./tree.png" />
         <SkillCircle></SkillCircle>
       </Layer>
