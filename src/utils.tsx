7b11a9da1d514d0273495b361bf66bbbe9e517cd
Omri Harel <omriharel@users.noreply.github.com>
2022-02-04 17:54:40 +0200

    add utilities for generating and reading official tree URLs

--- src/utils.tsx
+++ src/utils.tsx
@@ -87,3 +87,48 @@ export function parseBitList(encodedBitList: string): boolean[] {
 
   return bitList.slice(0, skillList.length)
 }
+
+export function encodePoeCompatibleTree(bitlist: boolean[]): string {
+  const originalTreeNodeIds: number[] = []
+
+  // map each toggled node to its original node ID
+  bitlist.forEach((toggled, index) => {
+    if (toggled) {
+      originalTreeNodeIds.push(skillList[index].originalTreeId)
+    }
+  })
+
+  // sort by ascending nodeId
+  originalTreeNodeIds.sort((a, b) => a - b)
+
+  // write header, node data and a trailing \00\00 to the buffer
+  const header = [0, 0, 0, 6, 0, 0, originalTreeNodeIds.length]
+  const buf = Buffer.alloc(originalTreeNodeIds.length * 2 + header.length + 2)
+
+  header.forEach((char, idx) => {
+    buf.writeUInt8(char, idx)
+  })
+
+  originalTreeNodeIds.forEach((nodeId, index) => {
+    buf.writeUInt16BE(nodeId, header.length + index * 2)
+  })
+
+  buf.writeUInt8(0, buf.length - 1)
+  buf.writeUInt8(0, buf.length - 2)
+
+  // base64-encode the result
+  return buf.toString('base64').replaceAll('+', '-').replaceAll('/', '_')
+}
+
+// this receives everything past the '/' in a poe.com passive tree URL
+export function decodePoeCompatibleTree(slug: string): boolean[] {
+  const buf = Buffer.from(decodeURIComponent(slug.replaceAll('-', '+').replaceAll('_', '/')), 'base64')
+  const treeNodeIdMap: { [key: number]: boolean } = {}
+
+  // skip the header and start reading
+  for (let i = 7; i < buf.length - 2; i += 2) {
+    treeNodeIdMap[buf.readUInt16BE(i)] = true
+  }
+
+  return skillList.map((node) => node.originalTreeId in treeNodeIdMap)
+}
